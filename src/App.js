353e33591a687eded7275e701d26f7d4294007ed
Akshad Kolhatkar <AkshadKolhatkar@gmail.com>
2021-02-24 16:04:47 +0530

    Timer-App-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import DisplayComponent from './Components/DisplayComponent'
+import BtnComponent from './Components/BtnComponent'
 import './App.css';
 
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import logo from './img/logo.png';
+
+
+
 function App() {
+  const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+        updatedM++;
+        updatedS = 0;
+    }
+    if(updatedMs === 60){
+      updatedS++;
+      updatedMs = 0;
+  }
+
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});      
+    
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    return setTime({ms:0, s:0, m:0, h:0});
+  };
+
+  const resume = () => start();
+
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+  const classes = useStyles();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+
+      <AppBar>
+        <Toolbar>
+        <img src={logo} style={{width:200, marginTop: -7}} />
+        </Toolbar>
+      </AppBar>
+
+      <div className="clock-holder">
+        <h1>Timer App</h1>
+        <div className="stopwatch">
+              <DisplayComponent time={time}/>
+              <BtnComponent status={status} reset={reset} resume={resume} stop={stop} start={start}/>  
+        </div>
+
+      </div>
+      <footer> <div id="Copyright">&#60;Crafted by Akshad Kolhatkar&#62;</div></footer>
     </div>
   );
 }
